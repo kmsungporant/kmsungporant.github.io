6d179648c321d3baa3f7f794919e27f14fefbb8e
Minsung Kim <69093400+kmsungporant@users.noreply.github.com>
2022-04-14 00:14:02 -0400

    remove header and gotoparrow

--- pages/about.tsx
+++ pages/about.tsx
@@ -1,6 +1,4 @@
 import Menu from "../components/menu";
-import Header from "../components/header";
-import Gotop from "../components/goTopArrow";
 import { useState } from "react";
 
 const AboutPage = () => {
@@ -9,9 +7,7 @@ const AboutPage = () => {
   return (
     <div className="h-full bg-DarkGray dark:bg-white scroll-smooth font-Consolas">
       <Menu menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
-      <Header menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
       <div></div>
-      <Gotop menuIsOpen={menuIsOpen} />
     </div>
   );
 };
--- pages/portfolio.tsx
+++ pages/portfolio.tsx
@@ -1,6 +1,5 @@
 import Menu from "../components/menu";
-import Header from "../components/header";
-import Gotop from "../components/goTopArrow";
+
 import { useState } from "react";
 
 const PortfolioPage = () => {
@@ -9,9 +8,7 @@ const PortfolioPage = () => {
   return (
     <div className="h-full bg-DarkGray dark:bg-white scroll-smooth font-Consolas">
       <Menu menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
-      <Header menuIsOpen={menuIsOpen} setMenuIsOpen={setMenuIsOpen} />
       <div></div>
-      <Gotop menuIsOpen={menuIsOpen} />
     </div>
   );
 };
