c6959f3bc78e23f7a05cc8c520b89881a2d67914
69093400+kmsungporant@users.noreply.github.com <69093400+kmsungporant@users.noreply.github.com>
2022-08-05 20:02:06 -0400

    animationg to sideInfo

--- components/sideInfo.tsx
+++ components/sideInfo.tsx
@@ -1,7 +1,36 @@
+import { motion } from "framer-motion";
+import Link from "next/link";
+
+const fromLeft = {
+  initial: {
+    x: "100%",
+    opacity: 0,
+  },
+  animate: {
+    x: 0,
+    opacity: 1,
+    transition: {
+      duration: 0.5,
+      ease: "easeInOut",
+      delay: 0.3,
+    },
+  },
+};
 export default function SideInfo() {
   return (
-    <div className="fixed -right-10 bottom-20">
-      <h1 className="text-white -rotate-90">{"</> by Minsung Kim"}</h1>
-    </div>
+    <motion.div
+      className="fixed -right-10 bottom-20"
+      variants={fromLeft}
+      initial="initial"
+      animate="animate"
+    >
+      <Link href="https://github.com/kmsungporant/minsung-web" passHref>
+        <a target="_blank">
+          <h1 className="-rotate-90 text-dark-secondary dark:text-light-secondary hover:text-dark-secondary/75 hover:dark:text-light-secondary/75">
+            {"</> by Minsung Kim"}
+          </h1>
+        </a>
+      </Link>
+    </motion.div>
   );
 }
