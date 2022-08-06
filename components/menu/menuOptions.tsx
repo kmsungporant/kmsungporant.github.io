679bf1b18c7d1ee7c7ceaed60ec9d2c5e9fb8ee8
69093400+kmsungporant@users.noreply.github.com <69093400+kmsungporant@users.noreply.github.com>
2022-08-06 00:59:45 -0400

    add animation to menuOptions

--- components/menu/menuOptions.tsx
+++ components/menu/menuOptions.tsx
@@ -38,7 +38,15 @@ export default function HeaderOptions({ setMenuIsOpen }) {
     <div className="h-full text-5xl font-black ">
       <ul className="flex flex-col items-center justify-center h-full leading-snug text-light-secondary dark:text-dark-secondary ">
         {categories.map((name, i) => (
-          <div key={i}>
+          <motion.div
+            key={i}
+            initial={{ opacity: 0, x: -100 }}
+            animate={{
+              opacity: 1,
+              x: 0,
+              transition: { duration: 0.5, ease: "easeInOut", delay: i * 0.15 },
+            }}
+          >
             <Link href={name.link} passHref>
               <li
                 className="cursor-pointer hover:dark:text-dark-secondary/75 hover:text-light-secondary/75"
@@ -47,18 +55,28 @@ export default function HeaderOptions({ setMenuIsOpen }) {
                 {name.name}
               </li>
             </Link>
-          </div>
+          </motion.div>
         ))}
 
         <div className="flex flex-row mt-4 space-x-4 ">
           {icons.map((icon, i) => (
             <Link href={icon.link} key={i}>
-              <a
+              <motion.a
                 target="_blank"
                 className="cursor-pointer hover:dark:text-dark-secondary/75 hover:text-light-secondary/75 "
+                initial={{ opacity: 0, y: 100 }}
+                animate={{
+                  opacity: 1,
+                  y: 0,
+                  transition: {
+                    duration: 0.5,
+                    ease: "easeInOut",
+                    delay: i * 0.15,
+                  },
+                }}
               >
                 <motion.div>{icon.icon}</motion.div>
-              </a>
+              </motion.a>
             </Link>
           ))}
         </div>
