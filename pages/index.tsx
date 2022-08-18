0495da5cffc64da4b6d72f37c24d32e138bb8f04
Minsung Kim <69093400+kmsungporant@users.noreply.github.com>
2022-08-18 01:46:59 -0400

    rebuild

--- pages/index.tsx
+++ pages/index.tsx
@@ -7,7 +7,7 @@ import Timeline from "../components/timeline";
 
 export default function Home() {
   return (
-    <div className="h-auto min-h-full bg-gray-800 lg:h-full space-y-44 lg:space-y-0 dark:bg-light-background font-Consolas">
+    <div className="h-auto min-h-full bg-gray-800 lg:h-full space-y-44 lg:space-y-0 dark:bg-light-background font-Consolas ">
       <MainPage />
       <AboutPage />
       <PorfolioPage />
