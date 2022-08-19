af3f9b17bca138b15adfc1cb6cd03465d634638c
Minsung Kim <69093400+kmsungporant@users.noreply.github.com>
2022-08-18 20:36:33 -0400

    add animation to cards portfolio

--- components/portfolio-page/newCards.tsx
+++ components/portfolio-page/newCards.tsx
@@ -78,7 +78,16 @@ export default function NewCards() {
         <div key={i}>
           {project.direction === "right" ? (
             <div className="relative flex items-center w-[100rem]  justify-start ">
-              <div className="absolute right-0 w-[55%] h-full text-right text-dark-primary dark:text-light-primary">
+              <motion.div
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                  transition: { duration: 1, delay: 0.5 },
+                }}
+                viewport={{ once: true }}
+                className="absolute right-0 w-[55%] h-full text-right text-dark-primary dark:text-light-primary"
+              >
                 <div className="flex flex-col justify-center h-full">
                   <h1 className="text-dark-secondary dark:text-light-secondary">
                     Featured Projects
@@ -106,16 +115,32 @@ export default function NewCards() {
                     <div className="m-5" />
                   )}
                 </div>
-              </div>
-              <img
+              </motion.div>
+              <motion.img
                 src={project.image}
                 alt={project.name}
                 className="object-cover w-[50%] h-full rounded-3xl"
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                  transition: { duration: 0.5 },
+                }}
+                viewport={{ once: true }}
               />
             </div>
           ) : (
             <div className="relative flex items-center w-[100rem] justify-end ">
-              <div className="absolute left-0 h-full text-left text-dark-primary dark:text-light-primary">
+              <motion.div
+                initial={{ opacity: 0, x: -100 }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                  transition: { duration: 1, delay: 0.5 },
+                }}
+                viewport={{ once: true }}
+                className="absolute left-0 h-full text-left text-dark-primary dark:text-light-primary"
+              >
                 <div className="flex flex-col w-[55%] justify-center h-full">
                   <h1 className="text-dark-secondary dark:text-light-secondary">
                     Featured Projects
@@ -143,11 +168,18 @@ export default function NewCards() {
                     <div className="m-5" />
                   )}
                 </div>
-              </div>
-              <img
+              </motion.div>
+              <motion.img
                 src={project.image}
                 alt={project.name}
                 className="object-cover w-[50%] h-full rounded-3xl "
+                initial={{ opacity: 0, x: 100 }}
+                whileInView={{
+                  opacity: 1,
+                  x: 0,
+                  transition: { duration: 0.5 },
+                }}
+                viewport={{ once: true }}
               />
             </div>
           )}
