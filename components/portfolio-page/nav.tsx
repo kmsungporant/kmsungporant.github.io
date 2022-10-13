e894209325c9dcafb0f6dd8cc05554ca23affda0
Minsung Kim <69093400+kmsungporant@users.noreply.github.com>
2022-10-13 01:03:25 -0400

    Fix z-index

--- components/portfolio-page/nav.tsx
+++ components/portfolio-page/nav.tsx
@@ -1,25 +1,32 @@
 import { motion } from "framer-motion";
 
 export default function Nav({ projects, selected, setSelected }) {
-
-    return (
-        <motion.div className="hidden lg:block "
-            initial={{ opacity: 0 }}
-            whileInView={{
-                opacity: 1,
-                transition: {
-                    duration: 1,
-                    ease: "easeIn",
-                },
-            }}
-            viewport={{ once: true }}
-        >
-            <div className="flex justify-center  ">
-                {projects.map((projects, i) => (
-                    <button onClick={() => (setSelected(i))} key={i} className={selected == i ? "z-50 p-2 mx-1 rounded-full bg-dark-primary  dark:bg-light-primary" : "p-2 mx-1 rounded-full bg-dark-secondary hover:bg-dark-secondary/70 dark:bg-light-secondary hover:dark:bg-light-secondary/70 z-50 "} />
-                ))}
-            </div >
-        </motion.div>)
-
-
+  return (
+    <motion.div
+      className="hidden lg:block "
+      initial={{ opacity: 0 }}
+      whileInView={{
+        opacity: 1,
+        transition: {
+          duration: 1,
+          ease: "easeIn",
+        },
+      }}
+      viewport={{ once: true }}
+    >
+      <div className="flex justify-center ">
+        {projects.map((projects, i) => (
+          <button
+            onClick={() => setSelected(i)}
+            key={i}
+            className={
+              selected == i
+                ? " p-2 mx-1 rounded-full bg-dark-primary  dark:bg-light-primary"
+                : "p-2 mx-1 rounded-full bg-dark-secondary hover:bg-dark-secondary/70 dark:bg-light-secondary hover:dark:bg-light-secondary/70 "
+            }
+          />
+        ))}
+      </div>
+    </motion.div>
+  );
 }
