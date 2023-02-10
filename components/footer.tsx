48522b16a6d89b2f72cef1e1eaf4cf4a7c8cfc90
Minsung Kim <69093400+kmsungporant@users.noreply.github.com>
2023-02-10 01:21:08 -0500

    add year

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,10 +1,11 @@
 import { SiFramer, SiNextdotjs, SiReact, SiTailwindcss } from "react-icons/si";
+let date = new Date().getFullYear();
 
 export default function Footer() {
   return (
     <div className="w-full bg-gray-900 h-[8%] font-yellowTail text-dark-primary dark:text-light-primary dark:bg-light-background transition-colors">
       <div className="flex flex-col items-center justify-center h-full ">
-        <span className="text-xs text-center">Copyright © 2022 Minsung Kim. All rights reserved.</span>
+        <span className="text-xs text-center">Copyright © {date} Minsung Kim. All rights reserved.</span>
         <span className="flex flex-row items-center space-x-2">
           <span className="">Powered by</span>
           <SiNextdotjs />, <SiReact />, <SiTailwindcss /> <span className="mr-2"> and</span>
